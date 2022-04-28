0944c4380a52828fa4321c21fd1d97aecce73591
JALE <jalemedlin@gmail.com>
2022-04-28 15:14:31 +0300

    Step 1 added

--- src/test/java/JaleTest01.java
+++ src/test/java/JaleTest01.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class JaleTest01 {
+public class JaleTest01 {
+    public static void main(String[] args) {
+        // Step 1 added to Test01
+        // Step 2 added to Test01
+    }
 }
